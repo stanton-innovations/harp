f541be6c7345d1054381ea2c1dc5b4e999e7f2a5
dartavion <richardranke@gmail.com>
2018-04-13 16:06:40 -0400

    basic components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,14 +5,28 @@ import { AppComponent } from './app.component';
 import { FakeBackendProvider } from './services/fake-backend-interceptor.service';
 import { HttpClientModule } from '@angular/common/http';
 import { TinkerService } from './services/tinker.service';
+import { HeaderComponent } from './containers/header/header.component';
+import { StoreComponent } from './components/store/store.component';
+import { SearchComponent } from './components/search/search.component';
+import { StoreLocationComponent } from './components/store-location/store-location.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import {MatExpansionModule, MatExpansionPanelHeader, MatGridListModule} from '@angular/material';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    StoreComponent,
+    SearchComponent,
+    StoreLocationComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    MatGridListModule,
+    BrowserAnimationsModule
   ],
   providers: [FakeBackendProvider, TinkerService],
   bootstrap: [AppComponent]
