6a7122bcae301713da8280d41402cb214526b34b
dartavion <richardranke@gmail.com>
2018-04-15 14:34:04 -0400

    add failing load search results success reducer test

--- src/app/store/reducers/search.reducer.spec.ts
+++ src/app/store/reducers/search.reducer.spec.ts
@@ -1,5 +1,6 @@
 import * as fromSearch from './search.reducer';
 import * as fromActions from '../actions/search.action';
+import { SearchResult } from '../../models/search-result';
 
 describe('Search Reducer', () => {
   it('has a default state', () => {
@@ -17,5 +18,25 @@ describe('Search Reducer', () => {
     expect(state.loading).toEqual(true);
     expect(state.loaded).toEqual(false);
     expect(state.entities).toEqual({});
+  });
+
+  it('LoadSearchResultsSuccess action', () => {
+    const searchResults: SearchResult[] = [
+      {id: 0, name: 'Hammer'},
+      {id: 1, name: 'Nail'}
+    ];
+
+    const entities = {
+      0: searchResults[0],
+      1: searchResults[1]
+    };
+
+    const { initialState } = fromSearch;
+    const action = fromActions.LoadSearchResultsSuccess;
+    const state = fromSearch.reducer(initialState, action);
+
+    expect(state.loaded).toEqual(true);
+    expect(state.loading).toEqual(false);
+    expect(state.entities).toEqual(entities);
   });
 });
