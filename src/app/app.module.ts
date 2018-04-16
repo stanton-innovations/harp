1e131ed36e168c26fc888ca2ebef2b7bc5c3d549
dartavion <richardranke@gmail.com>
2018-04-16 12:45:14 -0400

    organizing imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,26 +4,28 @@ import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FlexLayoutModule } from '@angular/flex-layout';
-import { RouterModule } from '@angular/router';
+import { EffectsModule } from '@ngrx/effects';
+import { MetaReducer, StoreModule } from '@ngrx/store';
+import { RouterStateSerializer, StoreRouterConnectingModule } from '@ngrx/router-store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { storeFreeze } from 'ngrx-store-freeze';
 
 import { AppComponent } from './app.component';
-import { FakeBackendProvider } from './services/fake-backend-interceptor.service';
-import { TinkerService } from './services/tinker.service';
 import { HeaderComponent } from './containers/header/header.component';
 import { StoreComponent } from './components/store/store.component';
 import { SearchComponent } from './components/search/search.component';
 import { StoreLocationComponent } from './components/store-location/store-location.component';
 import { ProfileComponent } from './components/profile/profile.component';
-import { SearchResultsModule } from './modules/search-results/search-results.module';
+
+import { environment } from '../environments/environment';
+import { reducers, CustomSerializer } from './store';
+
 import { AppRoutingModule } from './app-routing.module';
 import { MiniCartModule } from './modules/mini-cart/mini-cart.module';
-import { EffectsModule } from '@ngrx/effects';
-import { MetaReducer, StoreModule } from '@ngrx/store';
-import { environment } from '../environments/environment';
-import { storeFreeze } from 'ngrx-store-freeze';
-import { reducers, CustomSerializer } from './store';
-import { RouterStateSerializer, StoreRouterConnectingModule } from '@ngrx/router-store';
-import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { SearchResultsModule } from './modules/search-results/search-results.module';
+
+import { FakeBackendProvider } from './services/fake-backend-interceptor.service';
+import { TinkerService } from './services/tinker.service';
 
 export const metaReducers: MetaReducer<any>[] = !environment.production
   ? [storeFreeze]
