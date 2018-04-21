b94f73c5f920a46c7c099cd333c11dc7b1f5b469
dartavion <richardranke@gmail.com>
2018-04-21 17:27:06 -0400

    fix for compiling

--- src/app/modules/search-results/containers/search-results/search-results.component.ts
+++ src/app/modules/search-results/containers/search-results/search-results.component.ts
@@ -29,7 +29,7 @@ export class SearchResultsComponent implements OnInit {
     });
   }
 
-  showDetail(result: object) {
+  showDetail(result: any) {
     this.router.navigate(['detail'], {
       skipLocationChange: true,
       queryParams: {
